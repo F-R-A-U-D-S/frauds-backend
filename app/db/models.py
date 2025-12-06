b5e321ef47194f4dfce08c51e08d6dd2f33e1b94
tanasije286 <tanasije286@gmail.com>
2025-12-06 18:00:11 -0500

    Updated models for user

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, String, DateTime, Boolean
+from sqlalchemy import Column, String, DateTime, Boolean, Integer
 import uuid
-import datetime
+from datetime import datetime
 
 from app.db.base_class import Base
 
@@ -8,7 +8,38 @@ from app.db.base_class import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
+    employee_number = Column(Integer, unique=True, nullable=False)
+    name = Column(String, nullable=False)
     username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    title = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
     is_admin = Column(Boolean, nullable=False, default=False)
+
+from sqlmodel import SQLModel
+
+class UserBase(SQLModel):
+    name: str 
+    username: str 
+    title: str 
+
+
+class UserPublic(UserBase):
+    employee_number: int
+    id: str
+    is_admin: bool
+    created_at: datetime
+
+
+
+class UserCreate(UserBase):
+    employee_number: int
+    password_hash: str
+
+
+class UserUpdate(UserBase):
+    name: str | None = None
+    username: str | None = None
+    password_hash: str | None = None
+    title: str | None = None
+    is_admin: bool | None = None
