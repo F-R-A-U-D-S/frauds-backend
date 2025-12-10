f22365b189b2484308479adc5199c28c6f79a2df
Matt N <97564308+afk-m@users.noreply.github.com>
2025-12-10 10:57:13 -0500

    Allow all origins in middleware

--- app/main.py
+++ app/main.py
@@ -12,11 +12,7 @@ app = FastAPI(title="F.R.A.U.D.S Backend API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",  
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",  
-        "http://127.0.0.1:5174"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
