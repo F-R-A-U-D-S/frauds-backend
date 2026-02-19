f3cad0e4c0e839b0fcfd77eaeff29d07a2c557ca
tanasije286 <tanasije286@gmail.com>
2026-02-19 11:04:03 -0500

    Changed api calls for fraud breakdown

--- app/routes/report.py
+++ app/routes/report.py
@@ -6,14 +6,14 @@ from app.services import report_service
 router = APIRouter(prefix="/report", tags=["report"])
 service = report_service
 
-"""
-@router.get("/csv/{report_id}")
-def read_csv_report(report_id: int, user = Depends(get_current_user)):
-    report = service.get_report(report_id)
-    return report
-"""
-@router.get("/fraud_breakdown/{key:path}")
-async def fraud_breakdown(key: str, user = Depends(get_current_user)):
-    return service.get_fraud_breakdown(key)
+@router.get("/fraud_status_breakdown/{key:path}")
+async def fraud_status_breakdown(key: str, user = Depends(get_current_user)):
+    return service.get_fraud_status_breakdown(key)
+
+@router.get("/fraud_type_breakdown/{key:path}")
+async def fraud_type_breakdown(key: str, user = Depends(get_current_user)):
+    return service.get_fraud_type_breakdown(key)
+
+
     
 
