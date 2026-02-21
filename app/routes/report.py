00c49e20146d6dc4cbfbf26e2171ea228dd4e1f2
tanasije286 <tanasije286@gmail.com>
2026-02-20 20:48:13 -0500

    Added route for table breakdown

--- app/routes/report.py
+++ app/routes/report.py
@@ -6,6 +6,10 @@ from app.services import report_service
 router = APIRouter(prefix="/report", tags=["report"])
 service = report_service
 
+@router.get("/fraud_table_breakdown/{key:path}")
+async def fraud_breakdown(key: str, user = Depends(get_current_user)):
+    return service.get_fraud_table_breakdown(key)
+
 @router.get("/fraud_status_breakdown/{key:path}")
 async def fraud_status_breakdown(key: str, user = Depends(get_current_user)):
     return service.get_fraud_status_breakdown(key)
