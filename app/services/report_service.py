5bbb73fbf6cb07c960de2a465c167fe8a2f1637d
tanasije286 <tanasije286@gmail.com>
2026-02-22 23:12:42 -0500

    Updated pdf

--- app/services/report_service.py
+++ app/services/report_service.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 import io
 import csv
 import pandas as pd
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
+from reportlab.platypus import Image, SimpleDocTemplate, Spacer, Table, TableStyle, Paragraph
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 from app.core.local_storage import load_decrypted
+
+formattedDate = datetime.now().strftime("%B %d, %Y")
 
 def get_fraud_table_breakdown(key:str):
     csv_bytes = load_decrypted(key)
@@ -60,18 +63,52 @@ def get_csv_data_for_key(key: str) -> bytes:
     return data
 
 def convert_csv_to_pdf(csv_bytes: bytes) -> bytes:
+    
     csv_text = csv_bytes.decode("utf-8")
     reader = csv.reader(csv_text.splitlines())
     rows = list(reader)
 
     styles = getSampleStyleSheet()
 
-    reasoning_style = ParagraphStyle(
-        "ReasoningStyle",
+    report_header_style = ParagraphStyle(
+        "ReportHeaderStyle",
+        parent=styles["Title"],
+        alignment=0,
+        fontName="Times-Roman"
+    )
+    
+    report_paragraph_style = ParagraphStyle(
+        "ReportParagraphStyle",
         parent=styles["Normal"],
-        fontSize=9,
-        leading=11,        # line spacing
-        wordWrap="CJK",    # forces wrapping even for long strings
+        alignment=0,
+        fontName="Times-Roman"
+    )
+
+    table_header_style = ParagraphStyle(
+        "TableHeaderStyle",
+        parent=styles["Normal"],
+        fontName="Times-Roman"
+    )
+
+    amount_table_header_style = ParagraphStyle(
+        "AmountHeaderStyle",
+        parent=styles["Normal"],
+        alignment=2,
+        fontName="Times-Roman"
+    )
+
+    table_row_style = ParagraphStyle(
+        "TableRowStyle",
+        parent=styles["Normal"],      
+        wordWrap="CJK",
+        fontName="Times-Roman"
+    )
+
+    amount_table_row_style = ParagraphStyle(
+        "AmountRowStyle",
+        parent=styles["Normal"],
+        alignment=2,
+        fontName="Times-Roman"
     )
 
     if not rows:
@@ -79,57 +116,64 @@ def convert_csv_to_pdf(csv_bytes: bytes) -> bytes:
 
     header = rows[0]
 
-    # Find column indexes safely
     try:
         timeDate_idx = header.index("timestamp")
-        amount_idx = header.index("amount")
+        merchant_idx = header.index("merchant")
+        mcc_idx = header.index("mcc")
         reasoning_idx = header.index("reasoning")
+        amount_idx = header.index("amount")
     except ValueError as e:
         raise ValueError("Required columns missing from CSV") from e
 
-    # Build filtered table data
-    table_data = [["timestamp","amount","reasoning"]]
+    table_data = [[Paragraph("<b>Timestamp</b>",table_header_style),Paragraph("<b>Merchant</b>",table_header_style),Paragraph("<b>Fraud Reasoning</b>",table_header_style),Paragraph("<b>Amount</b>",amount_table_header_style)]]
 
     for row in rows[1:]:
         reasoning_value = row[reasoning_idx].strip() if len(row) > reasoning_idx else ""
 
-        if reasoning_value:  # non-null / non-empty
+        if reasoning_value:  
             table_data.append([
-                row[timeDate_idx],
-                row[amount_idx],
-                Paragraph(reasoning_value, reasoning_style),
+                Paragraph(str(row[timeDate_idx]), table_row_style),
+                Paragraph(row[merchant_idx] + " #" + str(int(float(row[mcc_idx]))), table_row_style),
+                Paragraph(reasoning_value, table_row_style),
+                Paragraph(row[amount_idx], amount_table_row_style)
             ])
 
-    # Handle case: nothing to show
     if len(table_data) == 1:
         table_data.append(["—", "No flagged results found"])
 
-    # PDF generation
     buffer = io.BytesIO()
-    doc = SimpleDocTemplate(buffer, pagesize=letter)
+    doc = SimpleDocTemplate(buffer, pagesize=letter, topMargin=25, leftMargin=25, rightMargin=25)
     styles = getSampleStyleSheet()
 
     elements = []
 
-    # Optional title
-    elements.append(Paragraph("<b>Fraud Analysis Summary</b>", styles["Title"]))
+    img = Image("fraud_logo_64.jpg")
+    img.hAlign = "LEFT"
+    elements.append(img)
+
+    elements.append(Spacer(1, 25))
+
+    elements.append(Paragraph("<b>Fraud Analysis Summary</b>",report_header_style))
+
+    formattedDate = datetime.now().strftime("%B %d, %Y")
+    elements.append(Paragraph("Report Date: " + formattedDate, report_paragraph_style))
+
+    elements.append(Paragraph("Fraudulent transactions are organized by review priority.", report_paragraph_style))
+
+    elements.append(Spacer(1, 10))
 
     table = Table(
         table_data,
-        colWidths=[100, 60, 340],  
+        colWidths=[90, 100, 300, 60],  
         repeatRows=1,
     )
 
     table.setStyle(TableStyle([
-        ("BACKGROUND", (0, 0), (-1, 0), colors.lightgrey),
-        ("GRID", (0, 0), (-1, -1), 0.5, colors.grey),
+        ("BACKGROUND", (0, 0), (-1, 0), "#b89fe3"),
+        ("LINEBELOW", (0,1), (-1,-1), 0.5, colors.grey, None, [2, 2]),
         ("FONT", (0, 0), (-1, 0), "Helvetica-Bold"),
         ("VALIGN", (0, 0), (-1, -1), "TOP"),
-        ("FONTSIZE", (0, 0), (-1, -1), 9),
-        ("LEFTPADDING", (0, 0), (-1, -1), 6),
-        ("RIGHTPADDING", (0, 0), (-1, -1), 6),
-        ("TOPPADDING", (0, 0), (-1, -1), 6),
-        ("BOTTOMPADDING", (0, 0), (-1, -1), 6),
+        ("FONTSIZE", (0, 0), (-1, -1), 8),
     ]))
 
     elements.append(table)
@@ -138,4 +182,4 @@ def convert_csv_to_pdf(csv_bytes: bytes) -> bytes:
 
     buffer.seek(0)
     return buffer.read()
-
+    
